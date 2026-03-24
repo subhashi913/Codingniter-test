e1683ead1f87086e0f63e53379a086aedd2a6713
subhashi913 <subhashini.c@geval6.com>
2026-03-24 16:14:30 +0530

    Create TaskModel.php

---
+++ task-feature-24-03-26/backend/TaskModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TaskModel extends Model
+{
+    protected $table = 'tasks';
+    protected $primaryKey = 'id';
+
+    protected $allowedFields = [
+        'title',
+        'description',
+        'status'
+    ];
+}
